ac229fa0740dcec563e7c8f0030d56a429c0167d
Jürgen Sieben <j.sieben@condes.de>
2016-07-08 19:14:44 +0200

    Bug fix on object rights script

--- PIT/core/parameters/grant_install_rights.sql
+++ PIT/core/parameters/grant_install_rights.sql
@@ -1,7 +1,7 @@
 prompt &s1.Grant select and references rights on parameter tables to &REMOTE_USER.
-grant select on parameter_tab to &REMOTE_USER.;
-grant select on parameter_group to &REMOTE_USER.;
-grant references on parameter_group to &REMOTE_USER.;
-grant select on parameter_type to &REMOTE_USER.;
-grant references on parameter_type to &REMOTE_USER.;
+grant select on &INSTALL_USER..parameter_tab to &REMOTE_USER.;
+grant select on &INSTALL_USER..parameter_group to &REMOTE_USER.;
+grant references on &INSTALL_USER..parameter_group to &REMOTE_USER.;
+grant select on &INSTALL_USER..parameter_type to &REMOTE_USER.;
+grant references on &INSTALL_USER..parameter_type to &REMOTE_USER.;
 
