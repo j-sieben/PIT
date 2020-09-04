42350eee99c4a4b77fc95ae1424318bc2c2e3b98
Jürgen Sieben <j.sieben@condes.de>
2020-09-04 16:06:38 +0200

    Create create_global_context.sql

---
+++ PIT/core/scripts/create_global_context.sql
@@ -0,0 +1,43 @@
+
+declare
+   l_context_exists number;
+   C_CTX constant varchar2(128 byte) := 'VERSION_CTX';
+begin
+   select count(*)
+     into l_context_exists
+     from dual
+    where exists(
+          select null
+            from dba_context
+           where namespace = upper(C_CTX));
+   
+   if l_context_exists = 0 then
+      execute immediate 'create context ' || C_CTX || ' using &UTIL_USER..utl_context accessed globally';
+      dbms_output.put_line('... Global context ' || C_CTX || ' created');
+   else
+      dbms_output.put_line('... Context ' || C_CTX || ' already exists');
+   end if;
+end;
+/
+
+
+declare
+   l_context_exists number;
+   C_CTX constant varchar2(128 byte) := 'KISMON_CTX';
+begin
+   select count(*)
+     into l_context_exists
+     from dual
+    where exists(
+          select null
+            from dba_context
+           where namespace = upper(C_CTX));
+   
+   if l_context_exists = 0 then
+      execute immediate 'create context ' || C_CTX || ' using &UTIL_USER..utl_context accessed globally';
+      dbms_output.put_line('... Global context ' || C_CTX || ' created');
+   else
+      dbms_output.put_line('... Context ' || C_CTX || ' already exists');
+   end if;
+end;
+/
