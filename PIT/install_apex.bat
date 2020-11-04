f553df62b2ef23defcc5142cb6a9af474f531995
Jürgen Sieben <j.sieben@condes.de>
2020-11-04 09:12:12 +0100

    Reduced requirement from SYSDBA user to ADMIN user

--- PIT/install_apex.bat
+++ PIT/install_apex.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p Credentials=Enter SYS-credentials without 'as sysdba':
+set /p Credentials=Enter ADMIN-credentials:
 set /p AppUser=Enter database user that is accessible by the APEX workspace:
 set /p APEXWorkspace=Enter APEX workspace where this application should be installed at:
 set /p AppAlias=Enter application alias for the PIT application:
@@ -7,6 +7,6 @@ set /p AppId=Enter application id for the PIT application:
 set /p DefLang=Enter default language (Oracle language name) for messages:
 set nls_lang=GERMAN_GERMANY.AL32UTF8
 
-sqlplus %Credentials% as sysdba @pit_install.sql %AppUser% %APEXWorkspace% %AppAlias% %AppId% %DefLang% 
+sqlplus %Credentials% @pit_apex_install.sql %AppUser% %APEXWorkspace% %AppAlias% %AppId% %DefLang% 
 
 pause
