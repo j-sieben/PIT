f01dfce5b03d0d0fb87557821ff3fcf8e541305f
Jürgen Sieben <j.sieben@condes.de>
2020-11-04 09:10:18 +0100

    Reduced requirement from SYSDBA user to ADMIN user

--- PIT/install.bat
+++ PIT/install.bat
@@ -1,9 +1,9 @@
 @echo off
-set /p Credentials=Enter SYS-credentials without 'as sysdba':
+set /p Credentials=Enter ADMIN-credentials:
 set /p InstallUser=Enter owner schema for PIT:
 set /p DefLang=Enter default language (Oracle language name) for messages:
 set nls_lang=GERMAN_GERMANY.AL32UTF8
 
-sqlplus %Credentials% as sysdba @pit_install.sql %InstallUser% %DefLang%
+sqlplus %Credentials% @pit_install.sql %InstallUser% %DefLang%
 
 pause
