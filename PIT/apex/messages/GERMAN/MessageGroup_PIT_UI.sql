f9496884dc13f98fe69b78aaa5295d4dc5eca287
Jürgen Sieben <j.sieben@condes.de>
2022-02-22 08:44:32 +0100

    Hotfix: PIT_UI messages added

--- PIT/apex/messages/GERMAN/MessageGroup_PIT_UI.sql
+++ PIT/apex/messages/GERMAN/MessageGroup_PIT_UI.sql
@@ -33,6 +33,24 @@ begin
     p_pms_description => q'^^',
     p_pms_pse_id => 60,
     p_pms_pml_name => 'GERMAN');
+
+  pit_admin.merge_message(
+    p_pms_name => 'PIT_UI_INVALID_REQUEST',
+    p_pms_pmg_name => 'PIT_UI',
+    p_pms_text => q'^Für den Request »#1#« ist kein Handler hinterlegt.^',
+    p_pms_description => q'^In einer Auswahlliste ist für den aktuellen Requestwert kein Entschiedungsbaum hinterlegt. Daher wird dieser Request nicht behandelt.^',
+    p_pms_pse_id => 30,
+    p_pms_pml_name => 'GERMAN',
+    p_error_number => -20000);
+
+  pit_admin.merge_message(
+    p_pms_name => 'PIT_UI_PARAMETER_REQUIRED',
+    p_pms_pmg_name => 'PIT_UI',
+    p_pms_text => q'^Parameter »#1#« darf nicht NULL sein.^',
+    p_pms_description => q'^Offensichtlich.^',
+    p_pms_pse_id => 30,
+    p_pms_pml_name => 'GERMAN',
+    p_error_number => -20000);
     
   commit;
   
