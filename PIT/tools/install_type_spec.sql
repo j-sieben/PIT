23b39ca0e9f59d3fcc0cab0e0facaef4322c42d1
Jürgen Sieben <j.sieben@condes.de>
2025-03-18 18:36:56 +0100

    Update install_type_spec.sql

--- PIT/tools/install_type_spec.sql
+++ PIT/tools/install_type_spec.sql
@@ -9,7 +9,7 @@ with has_table as (
           and type = 'TYPE'
           and name = upper('&1.'))
 select case amt when 0 then '&type_dir.&1..tps' else 'tools/null.sql' end script,
-       case amt when 0 then '&s1.Create type specification' || upper('&1.') else '&s1.Type specification ' || upper('&1.') || ' already exists' end msg
+       case amt when 0 then '&s1.Create type specification ' || upper('&1.') else '&s1.Type specification ' || upper('&1.') || ' already exists' end msg
   from has_table;
   
 set termout on
